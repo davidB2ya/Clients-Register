9b14a01ac8f4de4087825194ab2fa78e48385e69
davidB2ya <dbedoya0408@gmail.com>
2022-03-16 20:18:23 -0500

    make it better

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(allRoutes);
-app.use(express.static(path.join('../client/build')))
+app.use(express.static('../client/build'))
 
 // Setting
 const port = process.env.PORT || '3001'
